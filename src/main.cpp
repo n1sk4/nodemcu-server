96fee90eb1d7f28a3cf5ec02e7f4be84f969cab1
Nikša Zupčić <zupcic.niksa1@gmail.com>
2024-09-15 12:30:29 +0200

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,30 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <vector>
 #include <map>
 
 const char* ssid     = "12345678"; // WiFi SSID
 const char* password = "12345678"; // WiFi password 
+uint8_t PINS[] = {
+  D5, // GPIO14
+  D6, // GPIO12
+  D7  // GPIO13
+};
 
 std::map <uint8_t, String> leds = {
-  {D0, "OFF"},
-  {D1, "OFF"},
-  {D2, "OFF"}
+  {PINS[0], "OFF"},
+  {PINS[1], "OFF"},
+  {PINS[2], "OFF"}
 };
 std::map <String, uint8_t> led_web_names = {
-  {"LED1", D0},
-  {"LED2", D1},
-  {"LED3", D2}
+  {"LED1", PINS[0]},
+  {"LED2", PINS[1]},
+  {"LED3", PINS[2]}
 };
 std::map <String, uint8_t> led_web_colors = {
-  {"RED",   D0},
-  {"GREEN", D1},
-  {"BLUE",  D2}
+  {"RED",   PINS[0]},
+  {"GREEN", PINS[1]},
+  {"BLUE",  PINS[2]}
 };
 
 AsyncWebServer server(80);
@@ -31,7 +35,7 @@ String get_led_color(const String& var) {
   auto it = led_web_colors.find(var);
   if (it != led_web_colors.end()) {
     auto pin = it->second;
-    if (digitalRead(pin) == LOW) 
+    if (digitalRead(pin) == LOW) // Change (LOW/HIGH) depending on the LED configuration
       return it->first;
   }
   return "gray"; 
@@ -41,7 +45,7 @@ String led_control(const String& var) {
   auto it = led_web_names.find(var);
   if (it != led_web_names.end()) {
     auto pin = it->second;
-    if (digitalRead(pin) == LOW)
+    if (digitalRead(pin) == LOW) // Change (LOW/HIGH) depending on the LED configuration
       leds[pin] = "ON";
     else  
       leds[pin] = "OFF";
@@ -68,12 +72,20 @@ String processor(const String& var) {
   return String();
 }
 
+void server_on_led(uint8_t led_pin, uint8_t state, AsyncWebServerRequest *request){
+  digitalWrite(led_pin, state);
+  request->send(LittleFS, "/index.html", String(), false, processor);
+}
+
 void setup() {
   Serial.begin(115200);
   for (auto led : leds) {
     pinMode(led.first, OUTPUT);
-    digitalWrite(led.first, HIGH); 
+    digitalWrite(led.first, HIGH); // Change (LOW/HIGH) depending on the LED configuration
   }
+
+  Serial.print("\033[2J");  // Clear terminal
+  Serial.print("\033[H");   // Move cursor to the top-left corner
 
   if (!LittleFS.begin()) {
     Serial.println("An Error has occurred while mounting LittleFS");
@@ -86,8 +98,10 @@ void setup() {
     delay(1000);
     Serial.print(".");
   }
-
-  Serial.println(WiFi.localIP());
+  Serial.print("\n");
+  Serial.print("Client available at: ");
+  Serial.print(WiFi.localIP());
+  Serial.print("\n");
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(LittleFS, "/index.html", String(), false, processor);
@@ -98,37 +112,29 @@ void setup() {
   });
 
   server.on("/led1on", HTTP_GET, [](AsyncWebServerRequest *request) {
-    digitalWrite(D0, LOW);
-    request->send(LittleFS, "/index.html", String(), false, processor);
+    server_on_led(PINS[0], LOW, request); // Change (LOW/HIGH) depending on the LED configuration
   });
-
   server.on("/led1off", HTTP_GET, [](AsyncWebServerRequest *request) {
-    digitalWrite(D0, HIGH);
-    request->send(LittleFS, "/index.html", String(), false, processor);
+    server_on_led(PINS[0], HIGH, request); // Change (LOW/HIGH) depending on the LED configuration
   });
 
   server.on("/led2on", HTTP_GET, [](AsyncWebServerRequest *request) {
-    digitalWrite(D1, LOW);
-    request->send(LittleFS, "/index.html", String(), false, processor);
+    server_on_led(PINS[1], LOW, request); // Change (LOW/HIGH) depending on the LED configuration
   });
-
   server.on("/led2off", HTTP_GET, [](AsyncWebServerRequest *request) {
-    digitalWrite(D1, HIGH); 
-    request->send(LittleFS, "/index.html", String(), false, processor);
+    server_on_led(PINS[1], HIGH, request); // Change (LOW/HIGH) depending on the LED configuration
   });
 
   server.on("/led3on", HTTP_GET, [](AsyncWebServerRequest *request) {
-    digitalWrite(D2, LOW);
-    request->send(LittleFS, "/index.html", String(), false, processor);
+    server_on_led(PINS[2], LOW, request); // Change (LOW/HIGH) depending on the LED configuration
   });
-
   server.on("/led3off", HTTP_GET, [](AsyncWebServerRequest *request) {
-    digitalWrite(D2, HIGH);
-    request->send(LittleFS, "/index.html", String(), false, processor);
+    server_on_led(PINS[2], HIGH, request); // Change (LOW/HIGH) depending on the LED configuration
   });
 
   server.begin();
 }
 
 void loop() {
+  // do nothing
 }
